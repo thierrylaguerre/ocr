62c107f0cef87953645e5bed06a2e3cd5823e35a
thierrylaguerre <159148378+thierrylaguerre@users.noreply.github.com>
2024-05-27 23:39:47 +0200

    Update script.py

--- script.py
+++ script.py
@@ -2,30 +2,6 @@ import pytesseract
 from pytesseract import Output
 import PIL.Image
 import cv2
-"""
-Page segmentation modes:
-0 Orientation and script detection (OSD) only.
-1 Automatic page segmentation with OSD.
-2 Automatic page segmentation, but no OSD, or OCR.
-3 Fully automatic page segmentation, but no OSD. (Default
-4 Assume a single column of text of variable sizes.
-5 Assume a single uniform block of vertically aligned text
-6 Assume a single uniform block of text.
-7 Treat the image as a single text line.
-8 Treat the image as a single word.
-9 Treat the image as a single word in a circle.
-10 Treat the image as a single character.
-11 Sparse text. Find as much text as possible in no particular
-12 Sparse text with OSD.
-13 Raw line. Treat the image as a single text line,
-"""
-"""
-OCR Engine Mode
-0 Legacy engine only.
-1 Neural nets LSTM engine only.
-2 Legacy + LSTM engines.
-3 Default, based on what is available.
-"""
 myconfig = r"--psm 12 --oem 3"
 img = cv2.imread("texte.png")
 height, width, _ = img.shape
