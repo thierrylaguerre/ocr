c7c980c088f53cd909f6d5975fb897da05e02b6b
thierrylaguerre <159148378+thierrylaguerre@users.noreply.github.com>
2024-05-30 20:31:20 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,12 @@
 from paddleocr import PaddleOCR, draw_ocr
 from matplotlib import pyplot as plt
 import os
+from PIL import Image
+
+# Autoriser les doublons de la bibliothèque OpenMP
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-# Instanciation du modèle
+# Instanciation du modèle OCR
 ocr_model = PaddleOCR(lang='en', use_angle_cls=True, use_gpu=True)
 
 # Définition du chemin de l'image
@@ -16,15 +19,25 @@ if not os.path.exists(img_path):
 # Exécution de la méthode OCR sur le modèle OCR
 result = ocr_model.ocr(img_path)
 
-#handling the Tuple and list of lists
-inner_result = result[0]
-inner_result
-for res in inner_result:
-    print(res[1][0])
+# Extraction des boîtes, textes et scores détectés
+boxes = [line[0] for line in result[0]]
+texts = [line[1][0] for line in result[0]]
+scores = [line[1][1] for line in result[0]]
 
-# Extracting detected components
-boxes = [res[0] for res in result] # 
-texts = [res[1][0] for res in result]
-scores = [res[1][1] for res in result]
+# Utilisation de la police Arial ou une autre police disponible sur votre système
+font_path = "C:\\Windows\\Fonts\\arial.ttf"  
 
-font_path = os.path.join('PaddleOCR', 'doc', 'fonts', 'latin.ttf')
+# Chargement de l'image
+image = Image.open(img_path).convert('RGB')
+
+# Dessin des résultats OCR sur l'image
+im_show = draw_ocr(image, boxes, texts, scores, font_path=font_path)
+
+# Conversion de l'image pour l'affichage avec matplotlib
+im_show = Image.fromarray(im_show)
+
+# Affichage de l'image annotée
+plt.figure(figsize=(10, 10))
+plt.imshow(im_show)
+plt.axis('off')
+plt.show()
